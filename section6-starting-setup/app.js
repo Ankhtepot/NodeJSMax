310a31b7b52b8b1f0225078bd9e80e175592247b
PetrZavodny <petr.zavodny@chyronhego.com>
2021-08-27 07:46:23 +0200

    Up to lesson 91.

--- section6-starting-setup/app.js
+++ section6-starting-setup/app.js
@@ -2,8 +2,14 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
 
 const app = express();
+
+app.engine('hbs', expressHbs({ layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'})); //dosn't need to be set in this case, because views/layouts/ is default
+// app.set('view engine', 'pug');
+app.set('view engine', 'hbs');
+app.set('views', 'views'); // Not necessary, but would be if views are in other folder than "views"
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -15,7 +21,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404', { pageTitle: 'Page Not Found'});
 });
 
 app.listen(3000);
