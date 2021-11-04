d5a60473432cd94cffab8278de5e92a857f44550
PetrZavodny <petr.zavodny@chyronhego.com>
2021-11-04 07:53:33 +0100

    Up to lesson 191.

--- section12-mongoDb/models/product.js
+++ section12-mongoDb/models/product.js
@@ -1,38 +1,64 @@
-const mongoConnect = require('../util/database')
+const getDb = require('../util/database').getDb;
+const mongodb = require('mongodb');
 
 class Product {
-  constructor(title, price, description, imageUrl) {
-    this.title = title;
-    this.price = price;
-    this.description = description;
-    this.imageUrl = imageUrl;
-  }
+    constructor(title, price, description, imageUrl, id) {
+        this.title = title;
+        this.price = price;
+        this.description = description;
+        this.imageUrl = imageUrl;
+        this._id = id;
+    }
 
-  save() {
+    save() {
+        const db = getDb();
+        let dbOp;
+        if (this._id) {
+            dbOp = db.collection('products')
+                .updateOne(
+                {_id: new mongodb.ObjectId(this._id)},
+                { $set: this })
+        } else {
+            dbOp = db.collection('products')
+                .insertOne(this);
+        }
+        return dbOp.then(result => {
+            console.log(result);
+        })
+            .catch(err => {
+                console.log(err);
+            });
+    }
 
-  }
+    static fetchAll() {
+        const db = getDb();
+        return db
+            .collection('products')
+            .find()
+            .toArray()
+            .then(products => {
+                console.log(products);
+                return products;
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
+
+    static findById(productId) {
+        const db = getDb();
+        return db
+            .collection('products')
+            .find({_id: mongodb.ObjectId(productId)})
+            .next()
+            .then(product => {
+                console.log(product);
+                return product;
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
 }
-
-const Product = sequelize.define('product', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  title: Sequelize.STRING,
-  price: {
-    type: Sequelize.DOUBLE,
-    allowNull: false
-  },
-  imageUrl: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  description: {
-    type: Sequelize.STRING,
-    allowNull: false
-  }
-});
 
 module.exports = Product;
